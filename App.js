dfcda9bb901803854cc95b954f72c28a5ab7af1b
Ihor kulpekin <konstantin.boykov@pro-store.online>
2021-01-19 19:06:48 +0200

    done function calculate

--- App.js
+++ App.js
@@ -4,59 +4,59 @@ import InputView from "./src/components/InputView";
 import ViewButtons from "./src/components/ViewButtons";
 
 export default function App() {
-  const [result, setResult] = useState("0");
+  const [result, setResult] = useState("");
   const [operation, setOperation] = useState(null);
   const [prevValue, setPrevValue] = useState(null);
 
-  const calculate = (operation, value) => {
-    if (operation === "number") {
-      setResult(`${result}${value}`);
+  const calculate = (type, value) => {
+    if (type === "number") {
+      setResult(`${value}`);
     }
 
-    if (operation === "operator") {
+    if (type === "operator") {
       setOperation(value);
       setPrevValue(result);
-      setResult("0");
+      setResult(result);
     }
 
-    if (operation === "clear") {
+    if (type === "clear") {
       setResult("0");
       setOperation(null);
       setPrevValue(null);
     }
 
-    if (operation === "posneg") {
+    if (type === "posneg") {
       setResult(`${parseFloat(result) * -1}`);
     }
 
-    if(operation==='percentage'){
-      setResult(`${parseFloat(result)*0.01}`)
+    if (type === "percentage") {
+      setResult(`${parseFloat(result) * 0.01}`);
     }
 
-    if(operation === 'equal'){
+    if (type === "equal") {
       const current = parseFloat(result);
       const previous = parseFloat(prevValue);
 
-      if(operation === '+'){
-        setResult(previous+current);
+      if (operation === "+") {
+        setResult(previous + current);
         setOperation(null);
         setPrevValue(null);
       }
 
-      if(operation === '/'){
-        setResult(previous/current);
+      if (operation === "/") {
+        setResult(previous / current);
         setOperation(null);
         setPrevValue(null);
       }
 
-      if(operation === '—'){
-        setResult(previous-current);
+      if (operation === "—") {
+        setResult(previous - current);
         setOperation(null);
         setPrevValue(null);
       }
 
-      if(operation === 'x'){
-        setResult(previous*current);
+      if (operation === "x") {
+        setResult(previous * current);
         setOperation(null);
         setPrevValue(null);
       }
@@ -68,10 +68,7 @@ export default function App() {
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <InputView result={result}/>
-        <ViewButtons calculate={calculate}
-                     setOperation={setOperation}
-                     findFirst={findFirst}
-        />
+        <ViewButtons calculate={calculate}/>
       </View>
     </SafeAreaView>
   );
--- src/components/ViewButtons.js
+++ src/components/ViewButtons.js
@@ -2,40 +2,37 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import MyButton from "./MyButton";
 
-const ViewButtons = ({calculate, findFirst, setOperation}) => {
+const ViewButtons = ({calculate}) => {
   return (
     <View style={styles.container}>
       <View style={styles.row}>
-        <MyButton title="С" onPress={()=>{
-          setOperation('C');
-          calculate()
-        }}/>
-        <MyButton title="+/-" onPress={calculate}/>
-        <MyButton title="%" onPress={setOperation}/>
-        <MyButton title="/" onPress={setOperation} last={true}/>
+        <MyButton title="С" onPress={()=>calculate('clear')}/>
+        <MyButton title="+/-" onPress={()=>calculate('posneg')}/>
+        <MyButton title="%" onPress={()=>calculate('percentage')}/>
+        <MyButton title="/" onPress={()=>calculate('operator','/')} last={true}/>
       </View>
       <View style={styles.row}>
-        <MyButton title="7" onPress={()=>findFirst(7)}/>
-        <MyButton title="8" onPress={()=>findFirst(8)}/>
-        <MyButton title="9" onPress={()=>findFirst(9)}/>
-        <MyButton title="x" onPress={calculate} last={true}/>
+        <MyButton title="7" onPress={()=>calculate('number',7)}/>
+        <MyButton title="8" onPress={()=>calculate('number',8)}/>
+        <MyButton title="9" onPress={()=>calculate('number',9)}/>
+        <MyButton title="x" onPress={()=>calculate('operator','x')} last={true}/>
       </View>
       <View style={styles.row}>
-        <MyButton title="4" onPress={()=>findFirst(4)}/>
-        <MyButton title="5" onPress={()=>findFirst(5)}/>
-        <MyButton title="6" onPress={()=>findFirst(6)}/>
-        <MyButton title="—" onPress={setOperation} last={true}/>
+        <MyButton title="4" onPress={()=>calculate('number',4)}/>
+        <MyButton title="5" onPress={()=>calculate('number',5)}/>
+        <MyButton title="6" onPress={()=>calculate('number',6)}/>
+        <MyButton title="—" onPress={()=>calculate('operator','—')} last={true}/>
       </View>
       <View style={styles.row}>
-        <MyButton title="1" onPress={()=>findFirst(1)}/>
-        <MyButton title="2" onPress={()=>findFirst(2)}/>
-        <MyButton title="3" onPress={()=>findFirst(3)}/>
-        <MyButton title="+" onPress={()=>setOperation("+")} last={true}/>
+        <MyButton title="1" onPress={()=>calculate('number',1)}/>
+        <MyButton title="2" onPress={()=>calculate('number',2)}/>
+        <MyButton title="3" onPress={()=>calculate('number',3)}/>
+        <MyButton title="+" onPress={()=>calculate('operator','+')} last={true}/>
       </View>
       <View style={styles.row}>
-        <MyButton title="0" onPress={()=>findFirst(0)} zero={true}/>
-        <MyButton title="," onPress={calculate}/>
-        <MyButton title="=" onPress={calculate} last={true}/>
+        <MyButton title="0" onPress={()=>calculate('number',0)} zero={true}/>
+        <MyButton title="." onPress={()=>calculate('number','.')}/>
+        <MyButton title="=" onPress={()=>calculate('equal')} last={true}/>
       </View>
     </View>
   );
